0238f43503b2cd505a449849e06025cb22237179
mnicolas-2025361 <mnicolas-2025361@kinal.edu.gt>
2026-03-25 21:50:13 -0600

    feat: se agrega service de Usuario

--- src/main/java/com/example/kinalapp/Service/IUsuarioService.java
+++ src/main/java/com/example/kinalapp/Service/IUsuarioService.java
@@ -1,4 +1,22 @@
 package com.example.kinalapp.Service;
 
+import aj.org.objectweb.asm.Opcodes;
+import com.example.kinalapp.entity.Usuario;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IUsuarioService {
+    List<Usuario> listarUsuarios();
+    List<Usuario> listarUsuariosActivos();
+
+    Usuario guardar(Usuario usuario);
+
+    Optional<Usuario>buscarPorCodigoUsuario(String codigoUsuario);
+
+    Usuario actualizar(String codigoUsuario, Usuario usuario);
+
+    void eliminar(String codigoUsuario);
+
+    boolean existePorCodigoUsuario(String codigoUsuario);
 }
