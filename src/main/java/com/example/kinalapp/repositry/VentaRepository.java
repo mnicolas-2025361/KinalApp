2d411c9ba45c6c82fde7bf99e41db0eff5277683
mnicolas-2025361 <mnicolas-2025361@kinal.edu.gt>
2026-03-27 09:35:54 -0600

    fix: se corrigue de int a long en el repository de venta

--- src/main/java/com/example/kinalapp/repositry/VentaRepository.java
+++ src/main/java/com/example/kinalapp/repositry/VentaRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface VentaRepository extends JpaRepository<Venta, String> {
-    List<Venta> findByEstado(int estado);
+public interface VentaRepository extends JpaRepository<Venta, Long> {
+    List<Venta> findByEstado(long estado);
 }
