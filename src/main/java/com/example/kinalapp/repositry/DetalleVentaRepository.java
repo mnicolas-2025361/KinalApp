17f44942a3442b4e48bebc3cb4439a0b3ba29800
mnicolas-2025361 <mnicolas-2025361@kinal.edu.gt>
2026-03-27 22:49:43 -0600

    feat: se agrega la capa repository de detalleventa

--- src/main/java/com/example/kinalapp/repositry/DetalleVentaRepository.java
+++ src/main/java/com/example/kinalapp/repositry/DetalleVentaRepository.java
@@ -3,9 +3,7 @@ package com.example.kinalapp.repositry;
 import com.example.kinalapp.entity.DetalleVenta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
-public interface DetalleVentaRepository extends JpaRepository <DetalleVenta, String> {
+public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, String> {
 
-    List<com.example.kinalapp.entity.DetalleVenta> findByEstado(Long estado);
 }
