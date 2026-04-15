d8eca91ca9f3ed39b5e346617a46061b8ab78ec2
mnicolas-2025361 <mnicolas-2025361@kinal.edu.gt>
2026-04-15 01:22:35 -0600

    fix: se agrega relaciones de la entidad usuario

--- src/main/java/com/example/kinalapp/entity/Usuario.java
+++ src/main/java/com/example/kinalapp/entity/Usuario.java
@@ -1,9 +1,8 @@
 package com.example.kinalapp.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 @Table(name = "usuario")
@@ -21,6 +20,10 @@ public class Usuario {
     private String rol;
     @Column
     private long estado;
+
+    @OneToMany(mappedBy = "usuario")
+    private List<Venta> ventas;
+
 
     public Usuario(){
     }
